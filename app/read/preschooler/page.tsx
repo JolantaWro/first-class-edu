ed3c359ed4cf8676e3ac36541b1c821837713dd5
JolantaWro <jolanta.woronowska@gmail.com>
2025-08-05 20:18:21 +0200

    add new photo

--- app/read/preschooler/page.tsx
+++ app/read/preschooler/page.tsx
@@ -20,7 +20,7 @@ const wordExercises: WordExercise[] = [
   { word: "b", options: ["d", "p", "b", "h"], correctIndex: 2, image: "https://res.cloudinary.com/dadi3oed4/image/upload/v1754068359/pexels-padli-772478_wmnvfs.jpg" },
   { word: "c", options: ["c", "s", "o", "a"], correctIndex: 0, image: "https://res.cloudinary.com/dadi3oed4/image/upload/v1754253691/pexels-pixabay-533342_cdhc5l.jpg" },
   { word: "d", options: ["r", "d", "s", "p"], correctIndex: 1, image: "https://res.cloudinary.com/dadi3oed4/image/upload/v1749320084/home_egan7x.jpg" },
-  { word: "e", options: ["a", "e", "i", "o"], correctIndex: 1, image: "https://res.cloudinary.com/dadi3oed4/image/upload/v1753486543/elephant_letter_e.jpg" },
+  { word: "e", options: ["a", "e", "i", "o"], correctIndex: 1, image: "https://res.cloudinary.com/dadi3oed4/image/upload/v1754417850/elf_nvrygs.png" },
   { word: "f", options: ["g", "f", "t", "b"], correctIndex: 1, image: "https://res.cloudinary.com/dadi3oed4/image/upload/v1753486544/fish_letter_f.jpg" },
   { word: "g", options: ["g", "q", "p", "b"], correctIndex: 0, image: "https://res.cloudinary.com/dadi3oed4/image/upload/v1753486544/giraffe_letter_g.jpg" },
   { word: "h", options: ["m", "n", "h", "t"], correctIndex: 2, image: "https://res.cloudinary.com/dadi3oed4/image/upload/v1753486544/hat_letter_h.jpg" },
